8e791c3a3807176b3d79e50207a6a503e7e98070
vipulcharkravarthy <saivipul15@msitprogram.net>
2019-12-07 11:40:29 +0530

    Assign task

---
+++ React/react-tutorial/src/Form.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+
+class Form extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.initialState = {
+            name: '',
+            job: ''
+        };
+
+        this.state = this.initialState;
+    }
+
+    handleChange = event => {
+        const { name, value } = event.target;
+
+        this.setState({
+            [name] : value
+        });
+    }
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        
+        this.props.handleSubmit(this.state);
+        this.setState(this.initialState);
+    }
+
+    render() {
+        const { name, job } = this.state; 
+
+        return (
+            <form onSubmit={this.onFormSubmit}>
+                <label>Name</label>
+                <input 
+                    type="text" 
+                    name="name" 
+                    value={name} 
+                    onChange={this.handleChange} />
+                <label>Job</label>
+                <input 
+                    type="text" 
+                    name="job" 
+                    value={job} 
+                    onChange={this.handleChange} />
+                <button type="submit">
+                    Submit
+                </button>
+            </form>
+        );
+    }
+}
+
+export default Form;
